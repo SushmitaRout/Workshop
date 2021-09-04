4c226564380db3419a8e2b583813ab50c0b45136
Sushmita <sushmita@leewayhertz.com>
2021-09-04 10:46:51 +0530

    workshop

---
+++ workshop17/src/app.js
@@ -0,0 +1,68 @@
+const { request } = require("express");
+const express = require("express");
+require("./db/conn");
+const Book = require("./models/book")
+
+const app = express();
+const port = process.env.PORT || 8000
+
+
+app.use(express.json());
+
+ app.post("/books", async (req, res) => {
+     try {
+
+         const user = new Book(req.body);
+         const create = await user.save();
+         res.send(user);
+     } catch (e) {
+         res.send(e);
+     }
+ })
+
+ app.put("/books/:id", async (req, res) => {
+    try {
+
+        const id = req.params.id;
+        
+        const updatebook = await Book.findOneAndUpdate({_id:id},{
+            $set:{
+               bookname:req.body.bookname,
+               authorname:req.body.authorname
+            }
+        })
+        res.send(updatebook);
+    } catch (e) {
+        res.send(e);
+    }
+
+
+
+})
+
+ app.get("/books" ,async(req,res)=>{
+     try{
+      const bookData = await Book.find();
+      res.send(bookData);
+     }catch(e){
+        res.send(e);
+     }
+ })
+
+ app.delete("/books/:id",async(req,res)=>{
+     try{
+        const id = req.params.id;
+        const delbook = await Book.findByIdAndDelete(id)
+        res.send(delbook);
+     }
+     catch(e){
+        res.send(e);
+     }
+
+ })
+
+
+
+app.listen(port, () => {
+    console.log(`connection is setup at ${port}`);
+})
---
+++ workshop17/src/config.js
@@ -0,0 +1,7 @@
+const MongoClient = require("mongodb");
+
+var dburl="mongodb://localhost:27017/";
+var dbname="booksapi";
+var collection="books";
+
+
